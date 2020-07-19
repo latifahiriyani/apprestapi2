1eddb014af99ea44bf4bb227c1753785cf954de2
latifahiriyani <latifahiriyani98@gmail.com>
2020-07-19 08:13:23 +0700

    restapi project uas

--- routes.js
+++ routes.js
@@ -6,51 +6,7 @@ module.exports = function (app) {
     app.route('/')
         .get(jsonku.index);
 
-   app.route('/tampil')
-       .get(jsonku.tampilsemuaobat);
 
-       app.route('/tampil/:id')
-       .get(jsonku.tampilberdasarkanid);
-
-       app.route('/tambah')
-       .post(jsonku.tambahObat);
-
-       app.route('/ubah')
-       .put(jsonku.ubahObat);
-
-       app.route('/hapus')
-        .delete(jsonku.hapusObat);
-
-        app.route('/tampiladmin')
-        .get(jsonku.tampilsemuaadmin);
-
-        app.route('/tampiladmin/:id')
-        .get(jsonku.tampiladminberdasarkanid);
-
-        app.route('/tambahadmin')
-        .post(jsonku.tambahDataAdmin);
-
-        app.route('/ubahadmin')
-        .put(jsonku.ubahDataAdmin);
-
-        app.route('/hapusadmin')
-        .delete(jsonku.hapusDataAdmin);
-
-        app.route('/tampilsupplier')
-        .get(jsonku.tampilsemuasupplier);
-
-        app.route('/tampilsupplier/:id')
-        .get(jsonku.tampilsupplierberdasarkanid);
-
-        app.route('/tambahsupplier')
-        .post(jsonku.tambahDataSupplier);
-
-        app.route('/ubahsupplier')
-        .put(jsonku.ubahDataSupplier);
-
-        
-        app.route('/hapussupplier')
-        .delete(jsonku.hapusDataSupplier);
 
 
 }
