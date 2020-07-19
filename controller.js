aa53d3f667378bae07a8811f96ff37eb0d7f705d
latifahiriyani <latifahiriyani98@gmail.com>
2020-07-19 07:15:09 +0700

    tambah data obat

--- controller.js
+++ controller.js
@@ -30,3 +30,24 @@ exports.tampilberdasarkanid= function (req, res) {
             }
         });
 };
+
+
+//menambahkan data obat
+exports.tambahObat = function (req, res) {
+    var kode_obat =req.body.kode_obat;
+    var nama_obat = req.body.nama_obat;
+    var jenis_obat = req.body.jenis_obat;
+    var harga = req.body.harga;
+    var jumlah_stok = req.body.jumlah_stok;
+    var tanggal_exp = req.body.tanggal_exp;
+
+    connection.query('INSERT INTO obat (kode_obat,nama_obat,jenis_obat,harga,jumlah_stok,tanggal_exp) VALUES(?,?,?,?,?,?)',
+        [kode_obat,nama_obat, jenis_obat, harga, jumlah_stok, tanggal_exp],
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Berhasil Menambahkan Data!", res)
+            }
+        });
+};
